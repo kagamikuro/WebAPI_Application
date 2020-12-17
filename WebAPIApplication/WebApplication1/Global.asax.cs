8bb447e642b1438b7a50a80a47b171d8ab3c11ec
abelwrg <abelw@outlook.com>
2020-12-17 10:27:22 +0800

    upload web api with winform

---
+++ WebAPIApplication/WebApplication1/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+
+namespace WebApplication1
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // 在应用程序启动时运行的代码
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            RouteTable.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = System.Web.Http.RouteParameter.Optional }
+);
+        }
+    }
+}
