0346024ade415a1648f9ffe133a6c38dba576819
kagamikuro <l>
2020-12-18 07:51:12 +0800

    upload WebAPI

---
+++ WebAPIApp/Controllers/ProductsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIApp.Models;
+
+namespace WebAPIApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+
+        static List<Product> products = new List<Product>()
+        {
+            new Product { Name = "American Dirt", Category = "Fiction",Rate = 4, Price = 125},
+            new Product { Name = "The Major of Casterbridge", Category = "Fiction",Rate = 5, Price = 200 },
+            new Product { Name = "Douglass", Category = "Fiction",Rate = 3, Price = 150 },
+            new Product { Name = "J.D.ROBB", Category = "Crime",Rate = 5, Price = 230 }
+        };
+
+
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            Console.WriteLine("get all products called");
+            Product product = new Product();
+            products.Add(product);
+
+            return products;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetProduct(String name)
+        {
+            var product = products.FirstOrDefault((p) => p.Name == name);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public bool Post([FromBody]Product obj)
+        {
+            Product product = new Product();
+            product.Name = obj.Name;
+            product.Category = obj.Category;
+            product.Rate = obj.Rate;
+            product.Price = obj.Price;
+            products.Add(obj);
+            Console.WriteLine("add called");
+            Console.WriteLine(product);
+
+            return true;
+        }
+
+        [HttpDelete]
+        public bool Delete(String Name)
+        {
+            return products.Remove(products.Find(p => p.Name == Name));
+        }
+
+
+        [HttpPut]
+         public bool PutOne(Product model)
+         {
+             Product editModel = products.Find(p => p.Name == model.Name);
+             editModel.Name = model.Name;
+             editModel.Category = model.Category;
+             editModel.Rate = model.Rate;
+             editModel.Price = model.Price;
+             return true;
+         }
+
+}
+}
---
+++ WebAPIApp/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIApp.Models
+{
+    public class Product
+    { 
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Rate { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
